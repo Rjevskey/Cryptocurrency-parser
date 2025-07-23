10a46871aab62e203b234e17b515611f1c003a77
Rjevskey <yamschikov.vania@yandex.ru>
2025-07-23 23:50:24 +0300

    work-project 23.07.25

--- script.js
+++ script.js
@@ -1,51 +1,26 @@
-const cryptoSlider = new Swiper('.crypto-slider', {
-  loop: true,
-  slidesPerView: 'auto', // Фиксированное количество
-  centeredSlides: false, // Отключаем центрирование
+const swiper = new Swiper('.crypto-slider', {
+  loop: true, 
+  slidesPerView: 2, // Показываем 2 карточки
   spaceBetween: 20,
-  speed:800,
-  slideToClickedSlide: true,
-  slidesOffsetBefore: 0, 
-  slidesOffsetAfter: 0,
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  
-  effect: 'slide',
-  
-  on: {
-    // При переключении вправо - выравниваем по левому краю
-    slideNextTransitionStart: function() {
-      this.params.centeredSlides = false;
-      this.update();
-    },
-    // При переключении влево - центрируем слайды
-    slidePrevTransitionStart: function() {
-      this.params.centeredSlides = true;
-      this.update();
-    }
+    pagination: {
+    el: '.swiper-pagination',  // Селектор контейнера для точек
+    clickable: true,
+    type: 'bullets',
+    dynamicBullets: true, 
   },
 
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
   breakpoints: {
-    320: {
-      slidesPerView: 1,
-      centeredSlides: true
-    },
     768: {
-      slidesPerView: Auto,
-    },
-    1024: {
-      slidesPerView: 3.5,
-      spaceBetween: 10
+      slidesPerView: 2
     }
   }
 });
