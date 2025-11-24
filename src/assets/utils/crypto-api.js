b58ac56bce3a199aa5e9f69938022618ad1fab10
Ivan <yamschikov.vania@yandex.ru>
2025-11-24 13:02:51 +0300

    feat: add crypto API fetch function (24.11.12)

---
+++ src/assets/utils/crypto-api.js
@@ -0,0 +1,7 @@
+function requestApi() {
+    let receivedRequest = fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cripple%2Cbinancecoin%2Csolana%2Cusd-coin%2Ctron%2Cdogecoin&vs_currencies=usd')
+    .then({
+        receivedRequest.json()
+
+    })
+}
