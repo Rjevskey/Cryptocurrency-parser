9e00a346a017f6672e7648d57112ccc5ba1b5eed
Ivan <yamschikov.vania@yandex.ru>
2025-12-04 17:27:02 +0300

    refactor: rewrite currency API to return data and begin Vue integration (04.12.2025)

--- src/api/currency-api.js
+++ src/api/currency-api.js
@@ -1,18 +1,9 @@
 export function currencyApi() {
 	return fetch('https://api.exchangerate-api.com/v4/latest/USD')
 		.then((response) => response.json())
-		.then((data) => {
-			console.log('Данные для таблицы денежных валют получены:', data)
-
-			document.querySelector('.table__price--rub').textContent = data.rates.RUB
-			document.querySelector('.table__price--cad').textContent = data.rates.CAD
-			document.querySelector('.table__price--eur').textContent = data.rates.EUR
-			document.querySelector('.table__price--cny').textContent = data.rates.CNY
-			document.querySelector('.table__price--gbp').textContent = data.rates.GBP
-			document.querySelector('.table__price--jpy').textContent = data.rates.JPY
-			document.querySelector('.table__price--chf').textContent = data.rates.CHF
-			document.querySelector('.table__price--try').textContent = data.rates.TRY
-			document.querySelector('.table__price--aed').textContent = data.rates.AED
+		.then((dataPackage) => {
+			console.log('Данные для таблицы денежных валют получены:', dataPackage)
+			return dataPackage.rates
 		})
 		.catch((error) => {
 			console.log('Ошибка загрузки данных валют:', error)
--- src/components/CurrencyTable.vue
+++ src/components/CurrencyTable.vue
@@ -4,7 +4,7 @@ import { onMounted } from 'vue'
 import { currencyApi } from '@/api/currency-api'
 
 onMounted(() => {
-	currencyApi()
+	let dataPackage = currencyApi()
 })
 
 const currencies = ref([
@@ -50,7 +50,7 @@ const currencies = ref([
 
 <template>
 	<table class="table">
-		<tr v-for="currency in currencies" :key="currency.code">
+		<tr>
 			<th>
 				<h4 class="table__title">Currency</h4>
 			</th>
@@ -59,147 +59,15 @@ const currencies = ref([
 			</th>
 		</tr>
 
-		<tr>
+		<tr v-for="currency in currencies" :key="currency.code">
 			<td>
 				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-rub.png"
-						alt="image-rub"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Russian ruble,(RUB)</p>
+					<img :src="currency.icon" alt="image-rub" class="table__icons" />
+					<p class="table__nameCoin">{{ currency.name }}</p>
 				</div>
 			</td>
 			<td>
 				<p class="table__price--rub">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-usd.png"
-						alt="image-usd"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Canadian dollar, (CAD)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--cad">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-eur.png"
-						alt="image-eur"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">EURO, (EUR)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--eur">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-yuan.png"
-						alt="image-yuan"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Yuan, (CNY)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--cny">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-gbp.png"
-						alt="image-gbp"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Pound sterling, (GBP)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--gbp">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-yuan.png"
-						alt="image-eyn"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Japanese Yen, (JPY)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--jpy">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-swissFranc.png"
-						alt="image-swissFranc"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Swiss Franc, (CHF)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--chf">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-try.png"
-						alt="image-try"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">Turkish Lira, (TRY)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--try">N/A</p>
-			</td>
-		</tr>
-
-		<tr>
-			<td>
-				<div class="table__cell">
-					<img
-						src="/src/assets/img/lcon-coin/icon-currency/icon-dirham.png"
-						alt="image-dirham"
-						class="table__icons"
-					/>
-					<p class="table__nameCoin">UAE dirham, (AED)</p>
-				</div>
-			</td>
-			<td>
-				<p class="table__price--aed">N/A</p>
 			</td>
 		</tr>
 	</table>
