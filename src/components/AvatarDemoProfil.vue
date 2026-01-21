8d229bed20bd7c3f4104292f4b6421d320e50224
Rjevskey <yamschikov.vania@yandex.ru>
2026-01-21 23:55:25 +0300

    style: implement WelcomeBanner with avatar component and flex layout (21.01.2026)

---
+++ src/components/AvatarDemoProfil.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+</script>
+<template>
+	<div class="welcomeBanner__avatar">
+		<img
+			src="/img/banner-img/avatar-dog-byStolypin.png"
+			alt=""
+			class="welcomeBanner__avatar--img"
+		/>
+	</div>
+</template>
+
+<style></style>
--- src/components/WelcomeBanner.vue
+++ src/components/WelcomeBanner.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import AvatarDemoProfil from './AvatarDemoProfil.vue'
 </script>
 
 <template>
 	<div class="welcomeBanner">
 		<div class="welcomeBanner__border">
+			<AvatarDemoProfil />
 			<h3>Welcome back, Test User!</h3>
 		</div>
 	</div>
